f81e39ea4b4a25f9f28da19bdd6a1a3c23f2270b
JakeWright131 <36662802+JakeWright131@users.noreply.github.com>
2020-10-07 13:02:21 +0100

    Update bmi.js
    added comments

--- bmi.js
+++ bmi.js
@@ -8,12 +8,12 @@ class BMI {
 
         let bmi = this.weight / (this.height ** 2);
         return bmi;
-
+//outputs bmi to the console
 
     }
 
 }
-
+//inputs the values into the variables
 let MyBmi = new BMI(2, 68);
 let calculatedBMI = MyBmi.calculateBMI();
 console.log(calculatedBMI);
